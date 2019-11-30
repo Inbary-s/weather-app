6e582ae0b8037c70d0cd686d7854884fe58fcfc3
Inbary-s <inbarsim6@gmail.com>
2019-11-29 20:08:41 -0700

    fixed forecast!

--- assets/function.js
+++ assets/function.js
@@ -1,11 +1,10 @@
 var apiKey = "bcdd23b6dd6d8821691e02632d9ffdc8";
 var cities = [];
-
+var storageCities = localStorage.getItem('cities');
+var storageCitiesArr = (storageCities.split(','));
+ 
 // getItem from local storage upon load
 $(document).ready(function() {
-    var storageCities = localStorage.getItem('cities');
-    var storageCitiesArr = (storageCities.split(','));
-    console.log(storageCitiesArr);
     // Rerender cities button arr
     for (var i = 0; i < storageCitiesArr.length; i++) {
         var a = $("<button>");
@@ -26,12 +25,10 @@ $("#sBtn").on("click", function(event) {
   handleAPI($("#search").val());
   // Save to localStorage
   localStorage.setItem("cities", cities); 
-  console.log("you stored ", localStorage.getItem("cities", cities));
 });
 // City buttons functionality
 $(document).on('click', '.city', function(e){
     e.preventDefault()
-    console.log($(this).attr('data-name'));
     handleAPI($(this).attr('data-name'));
 })
 // get all weather info
@@ -42,7 +39,6 @@ handleAPI = term => {
     url: queryURLNew,
     method: "GET"
   }).then(function(response) {
-    console.log(response);
     $(cityInfo).empty();
     var name = response.name;
     var iconCode = response.weather[0].icon;
@@ -57,7 +53,6 @@ handleAPI = term => {
     $("#cityInfo").html(cityName);
     $("#humidity").text("Humidity: " + response.main.humidity);
     $("#wind").text("Wind Speed: " + response.wind.speed);
-    console.log(temp);
     // UV Index;
     queryURLuv = "http://api.openweathermap.org/data/2.5/uvi/forecast?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
     $.ajax({
@@ -65,30 +60,20 @@ handleAPI = term => {
       method: "GET"
     }).then(function(response) {
       var uvIndex = (response[0].value);
-      var uvIdT = $("#uv").text("UV Index: ");
-      var uvIdNum = $("#num").text(uvIndex);
-      console.log(uvIndex);
       uvColor();
       function uvColor(){
         if (uvIndex<2) {
           $('#num').css('background-color', 'rgba(0, 128, 0, 0.63)');
-          // $('#num').addClass('green');
         }else if (uvIndex<6 && uvIndex>2){
           $('#num').css('background', 'rgba(255, 208, 0, 0.658)');
-          // $('#num').addClass('yellow');
         }else if(uvIndex>6){
           $('#num').css('background', 'rgba(255, 0, 0, 0.616)');
-          // $('#num').addClass('red');
         }
       }
     });
     // 5 Day forecast
         var forecastTerm = (response.name);
-        // var forecastTerm = ($('#sBtn').val() || $('.city').attr('data-name'));
         var forecastArr = [];
-        console.log(forecastTerm);
-        console.log($('#search').val());
-        console.log($('.city').attr('data-name'));
         $.ajax({
             url: `http://api.openweathermap.org/data/2.5/forecast?q=${forecastTerm}&appid=${apiKey}&mode=json`,
             method: "GET"
@@ -97,7 +82,6 @@ handleAPI = term => {
             for (i = 5; i < 40; i += 8) {
                 forecastArr.push(data.list[i]);
                 if (i === 37) {
-                    console.log(forecastArr);
                     appendForecast(forecastArr);
                 }
             }
@@ -110,7 +94,6 @@ handleAPI = term => {
         $('#forecast').html('')
         arr.map(item => {
             var forecastDateArr = ((item.dt_txt).split(' '));
-            console.log(forecastDateArr);
             var forecastDate = forecastDateArr[0];
             $("#forecast").append(`<div class='card forecastCard'><h6>${forecastDate}</h6>
             <img src='http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png'>
@@ -131,7 +114,6 @@ function renderCities() {
     $(".cities").prepend(a);
     a.insertBefore(cities[i]);
     localStorage.setItem("cities", cities); 
-    console.log("you stored ", localStorage.getItem("cities", cities));
   }
 }
 renderCities();
